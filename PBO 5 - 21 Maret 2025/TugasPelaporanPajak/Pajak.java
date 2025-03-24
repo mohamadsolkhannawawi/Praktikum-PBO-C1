de2901abed29d4541b09ddb53bcaf485c92af5de
MohamadSolkhanNawawi <144540540+mohamadsolkhannawawi@users.noreply.github.com>
2025-03-24 20:39:32 +0700

    Update deskripsi Pajak.java

--- PBO 5 - 21 Maret 2025/TugasPelaporanPajak/Pajak.java
+++ PBO 5 - 21 Maret 2025/TugasPelaporanPajak/Pajak.java
@@ -1,3 +1,12 @@
+/* 
+ * Nama File    : Pajak.java
+ * Nama Pembuat : Mohamad Solkhan Nawawi
+ * NIM Pembuat  : 24060123120020
+ * Tanggal      : 21 Maret 2025
+ * Deskripsi    : Pajak merupakan interface yang digunakan pada class
+ *                Petani, Pengusaha, PNS.              
+*/
+
 package TugasPelaporanPajak;
 
 public interface Pajak {
