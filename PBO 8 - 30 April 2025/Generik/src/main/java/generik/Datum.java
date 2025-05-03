a9541b79c6f13f0b51372fb10b9a15038ecae072
MohamadSolkhanNawawi <144540540+mohamadsolkhannawawi@users.noreply.github.com>
2025-05-03 21:54:32 +0700

    Update Datum.java

--- PBO 8 - 30 April 2025/Generik/src/main/java/generik/Datum.java
+++ PBO 8 - 30 April 2025/Generik/src/main/java/generik/Datum.java
@@ -1,3 +1,8 @@
+// Nama file    : Datum.java
+// Nama Pembuat : Mohamad Solkhan Nawawi - 24060123120020
+// Tanggal      : 30/04/2025
+// Deskripsi    : Datum merupakan class generik dengan atribut isi dengan tipe generik T
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
