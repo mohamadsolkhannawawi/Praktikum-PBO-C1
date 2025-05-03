6f72633ce2b9392202fd2b14293136166e4d8e8b
MohamadSolkhanNawawi <144540540+mohamadsolkhannawawi@users.noreply.github.com>
2025-05-03 21:55:59 +0700

    Update ContohMetodeGenerik.java

--- PBO 8 - 30 April 2025/Generik/src/main/java/generik/ContohMetodeGenerik.java
+++ PBO 8 - 30 April 2025/Generik/src/main/java/generik/ContohMetodeGenerik.java
@@ -1,3 +1,9 @@
+// Nama file    : ContohMetodeGenerik.java
+// Nama Pembuat : Mohamad Solkhan Nawawi - 24060123120020
+// Tanggal      : 30/04/2025
+// Deskripsi    : ContohMetodeGenerik merupakan class dengan implementasi generik pada methodnya,
+//                yang dapat mengendalikan class Datum
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
